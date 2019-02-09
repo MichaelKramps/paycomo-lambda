cbafc7a8da8ae66ae4fc938d2c40f31fc96a6d0a
michaelkramps <mdk989@gmail.com>
2019-02-09 06:57:36 -0600

    updated s3 class to accept a SNSEvent object

--- src/main/java/publicApi/UploadToS3.java
+++ src/main/java/publicApi/UploadToS3.java
@@ -7,14 +7,41 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.PaycomoTransactionS3Request;
 
-public class UploadToS3 implements RequestHandler<PaycomoTransactionS3Request, String> {
-    public String handleRequest(PaycomoTransactionS3Request request, Context context) {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UploadToS3 implements RequestHandler<SNSEvent, String> {
+    public String handleRequest(SNSEvent request, Context context) {
         String accessKeyId = System.getenv("S3_ACCESS_KEY");
         String secretKey = System.getenv("S3_SECRET_KEY");
+
+        SNSEvent.SNS sns = request.getRecords().get(0).getSNS();
+        ObjectMapper mapper = new ObjectMapper();
+        PaycomoTransactionS3Request s3Request;
+        try {
+            s3Request = mapper.readValue(sns.getMessage(), PaycomoTransactionS3Request.class);
+        } catch (Exception e){
+            s3Request = new PaycomoTransactionS3Request();
+            s3Request.setBucketName("paycomo-transactions");
+            s3Request.setContent("There was a problem parsing the SNS object");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyy-mm-dd_hh:mm:ss:SSS");
+            try{
+                s3Request.setDisplayName("s3error-" + dateFormat.parse(new Date().toString()));
+            } catch (ParseException p){
+                s3Request.setDisplayName("dateParseError");
+                s3Request.setContent("There was a problem parsing the SNS object and parsing the Date");
+            }
+        }
+
+        System.out.println("bucketName: " + s3Request.getBucketName());
+        System.out.println(request.toString());
 
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretKey);
 
@@ -25,18 +52,22 @@ public class UploadToS3 implements RequestHandler<PaycomoTransactionS3Request, S
 
         try {
             // Upload a text string as a new object.
-            s3Client.putObject(request.getBucketName(), request.getDisplayName(), request.getContent());
+            s3Client.putObject(s3Request.getBucketName(), s3Request.getDisplayName(), s3Request.getContent());
         }
         catch(AmazonServiceException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
             // it, so it returned an error response.
-            return "error";
+            System.out.println("Amazon Service Exception: ");
+            e.printStackTrace();
+            return "Amazon Service Exception";
         }
         catch(SdkClientException e) {
             // Amazon S3 couldn't be contacted for a response, or the client
             // couldn't parse the response from Amazon S3.
-            return "error 2";
+            System.out.println("SDK Client Exception: ");
+            e.printStackTrace();
+            return "SDK Client Exception";
         }
-        return "success";
+        return "Success";
     }
 }
